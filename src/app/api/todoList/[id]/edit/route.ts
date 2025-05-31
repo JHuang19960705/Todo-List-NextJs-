a7718699d024abe83a30a45c83e30099f01eacf5
“JHuang19960705 <abc850705@gmail.com>
2025-05-31 16:27:59 +0800

    v2_修正delete/route.ts

--- src/app/api/todoList/[id]/edit/route.ts
+++ src/app/api/todoList/[id]/edit/route.ts
@@ -2,32 +2,22 @@ import { NextRequest, NextResponse } from 'next/server';
 import pool from '@/lib/db';
 import { ResultSetHeader } from 'mysql2';
 
-export async function POST(
-  request: NextRequest,
-  { params }: { params: { id: string } }
-) {
-  const idNum = parseInt(params.id, 10);
+type Context = {
+  params: {
+    id: string;
+  };
+};
+
+export async function POST(request: NextRequest, context: Context) {
+  const idNum = parseInt(context.params.id, 10);
   if (isNaN(idNum)) {
     return NextResponse.json({ error: 'Invalid id' }, { status: 400 });
   }
 
-  const data = await request.formData();
-  const name = data.get('name');
-  const due_date = data.get('due_date');
-  const content = data.get('content');
-
-  if (
-    typeof name !== 'string' ||
-    typeof due_date !== 'string' ||
-    typeof content !== 'string'
-  ) {
-    return NextResponse.json({ error: 'Invalid input' }, { status: 400 });
-  }
-
   try {
     const [result] = await pool.query<ResultSetHeader>(
-      'UPDATE todoList SET name = ?, due_date = ?, content = ? WHERE id = ?',
-      [name, due_date, content, idNum]
+      'DELETE FROM todoList WHERE id = ?',
+      [idNum]
     );
 
     if (result.affectedRows === 0) {
