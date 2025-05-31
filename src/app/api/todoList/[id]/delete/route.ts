13337f050ef6eefe1a0d040397e977c3e8092741
“JHuang19960705 <abc850705@gmail.com>
2025-05-31 16:30:38 +0800

    v3_修正delete/route.ts

--- src/app/api/todoList/[id]/delete/route.ts
+++ src/app/api/todoList/[id]/delete/route.ts
@@ -2,10 +2,13 @@ import { NextRequest, NextResponse } from 'next/server';
 import pool from '@/lib/db';
 import { ResultSetHeader } from 'mysql2';
 
-export async function POST(
-  request: NextRequest,
-  context: { params: { id: string } }
-) {
+type Context = {
+  params: {
+    id: string;
+  };
+};
+
+export async function POST(request: NextRequest, context: Context) {
   const idNum = parseInt(context.params.id, 10);
   if (isNaN(idNum)) {
     return NextResponse.json({ error: 'Invalid id' }, { status: 400 });
